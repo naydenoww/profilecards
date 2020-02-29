98d59322690a9d7560cbab0d0f6947c698ccdf8a
Борислав Найденов <naydenov@isenselabs.com>
2020-02-29 13:38:27 +0200

    Initial version of the index page. Installed node sass.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  return(
+    <div className="card">
+      <h1>Personal information</h1>
+      <div className="button-wrapper">
+        <button className="btn secondary">Back</button>
+        <button className="btn primary">Next</button>
+      </div>
     </div>
   );
 }
